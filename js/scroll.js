2f3a8ed30bea145e6632ed4f49e2361731c9d3ab
Maxim <bhd.m@ya.ru>
2016-01-20 11:36:49 +0300

    add jquery & scroll

---
+++ js/scroll.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('a[href^="#"]').click(function(){ //берем все ссылки атрибут href которых начинается с #
+        if(document.getElementById($(this).attr('href').substr(1)) != null) { //на странице есть элемент с нужным нам id
+            $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 1100); // анимируем скролл к элементу
+        }
+        return false;
+    });
+});
